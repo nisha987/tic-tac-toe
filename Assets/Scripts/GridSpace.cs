a0272031b3732ac49dae48ef6cdd7295dfc0a684
nisha987 <nshekhawat1000@gmail.com>
2018-03-22 19:13:00 +0530

    tic-tac-toe

---
+++ Assets/Scripts/GridSpace.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class GridSpace : MonoBehaviour {
+
+    public Button button;
+    public Text buttonText;
+    
+
+    private GameController gameController;
+
+    public void setSpace()
+    {
+        buttonText.text = gameController.GetPlayerSide();
+        button.interactable = false;
+        gameController.EndTurn();
+    }
+
+    public void SetGameControllerReference(GameController controller)
+    {
+        gameController = controller;
+    }
+
+
+}
